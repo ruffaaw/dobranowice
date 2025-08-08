9d4c9fe091ef3bba75a91f535d0e522c9a8f4928
ruffaaw <rafalciupek07@gmail.com>
2025-08-08 22:31:41 +0200

    feat: updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import HomeSection from "../components/HomeSection";
 import Profits from "@/components/Profits";
 import Gallery from "@/components/Gallery";
+import Houses from "@/components/Houses";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HomeSection />
       <Investment />
       <Profits />
+      <Houses />
       <Gallery />
     </div>
   );
