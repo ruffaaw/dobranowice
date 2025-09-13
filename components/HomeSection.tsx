955e00cb6e36152a6fdd8b502d97759ed4d8aaec
ruffaaw <rafalciupek07@gmail.com>
2025-09-13 16:12:08 +0200

    feat: added animations

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -42,14 +42,26 @@ export default function HomeSection() {
         <div className="absolute inset-0 bg-gradient-to-t sm:bg-gradient-to-r from-black/70 via-black/40 via-20% to-transparent z-10" />
 
         {imageLoaded && (
-          <div className="absolute max-sm:bottom-10 sm:top-1/2 sm:left-12 lg:left-24 z-20 w-full sm:max-w-xl text-center sm:text-left sm:transform sm:-translate-y-1/2">
-            <h1 className="text-white text-3xl sm:text-5xl lg:text-6xl font-bold leading-snug drop-shadow-[2px_2px_4px_rgba(0,0,0,0.7)]">
+          <div
+            className="absolute max-sm:bottom-10 sm:top-1/2 sm:left-12 lg:left-24 z-20 w-full sm:max-w-xl text-center sm:text-left sm:transform sm:-translate-y-1/2"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
+            <h1
+              className="text-white text-3xl sm:text-5xl lg:text-6xl font-bold leading-snug drop-shadow-[2px_2px_4px_rgba(0,0,0,0.7)]"
+              data-aos="fade-right"
+              data-aos-delay="200"
+            >
               Nowoczesne osiedle domów <br /> w sąsiedztwie Wieliczki <br />
               <span className="text-[#457b9d]">Twój azyl spokoju</span> blisko
               miasta.
             </h1>
 
-            <div className="mt-6 flex flex-col sm:flex-row gap-4 items-center sm:items-start">
+            <div
+              className="mt-6 flex flex-col sm:flex-row gap-4 items-center sm:items-start"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
               <Link
                 href="#houses"
                 className="bg-[#1d3557] text-white text-lg sm:text-xl lg:text-2xl px-6 py-3 rounded-lg font-semibold shadow-lg transition hover:bg-[#e63946] w-fit"
